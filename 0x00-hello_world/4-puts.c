d9f5f2f0f4c84f3829fd5a386849681475682034
khadija <khadija.shamseldin@gmail.com>
2023-09-11 12:25:45 -0700

    my first c language code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main (void)
+/*this code will print a sentence*/
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	return (0);
 }
