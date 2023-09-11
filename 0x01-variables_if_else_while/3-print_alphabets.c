d8d4b5e39b9dfd0de04fe47a355f3c77d203de7c
khadija <khadija.shamseldin@gmail.com>
2023-09-11 15:43:34 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this program will prints alphabet
+ * Return:(0)
+ */
+int main(void)
+{
+	char c;
+
+	c = 97;
+	while (c < 123)
+	{
+		putchar((char)c), c++;
+	}
+	c = c / 2 + 4;
+	while (c < 91)
+	{
+		putchar((char)c), c++;
+	}
+	putchar('\n);
+	return (0);
+}
