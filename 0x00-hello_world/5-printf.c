b29ee89f11a7bdb13a9e750222c6764db5d7c3fd
khadija <khadija.shamseldin@gmail.com>
2023-09-11 12:58:20 -0700

    my second code in c language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this program is going to print a sentence
+ * Return: (0)
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art, %d\n");
+	return (0);
+}
