4097b65e410979a3638c92ebdd1ac8420dcfeb8a
khadija <khadija.shamseldin@gmail.com>
2023-09-11 13:25:42 -0700

    my third c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	long int k;
 	long long int e;
 	float z;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(v));
 	printf("Size of an int: %d byte(s)\n", sizeof(x));
 	printf("Size of a long int: %d byte(s)\n", sizeof(k));
