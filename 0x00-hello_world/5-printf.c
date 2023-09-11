44f556b089f56052162eb8ba3998002b3f09d35f
khadija <khadija.shamseldin@gmail.com>
2023-09-11 12:59:36 -0700

    my second code in c language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	prinf("with proper grammar, but the outcome is a piece of art, %d\n");
+	printf("with proper grammar, but the outcome is a piece of art, %d\n");
 	return (0);
 }
