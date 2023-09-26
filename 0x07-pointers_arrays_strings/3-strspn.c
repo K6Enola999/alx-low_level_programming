30a7f3101dde6e5dcee88859442150ded8a18043
khadija <khadija.shamseldin@gmail.com>
2023-09-25 17:35:11 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length pf a prefix substring
+ * @s: char pointer
+ * @accept: char pointer
+ * Return: number of bytes from accept in s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, found;
+	unsigned int n = 0;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		found = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				found = 1;
+			j++;
+		}
+		j = 0;
+		if (found == 0)
+			break;
+		i++;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x07.c-duble pointers and multidimensional arrays in c.
