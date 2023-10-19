fa826ce21d303705d2ed27b8908318dddabdbe12
khadija shamseldin <khadija.shamseldin@gmail.com>
2023-10-19 16:35:35 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nod - adds a new node at the beginning of a list_t list
+ * @head: pointer to the head of a linked list
+ * @str: string - malloc'ed string
+ * Return: address of the new element, or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(size_t));
+
+	if (new_head == NULL)
+		return (NULL);
+
+	new_head->str = strdup(str);
+	new_head->len = strlen(str);
+	new_head->next = *head;
+	*head = new_head;
+
+	return (new_head);
+}
