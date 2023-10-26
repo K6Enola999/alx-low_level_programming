4ee22149741e34ea7da481018b25b89723cc2e09
khadija shamseldin <khadija.shamseldin@gmail.com>
2023-10-26 12:46:50 -0700

    bitwise operators

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits needed to flip to 
+ * get from one number to another
+ * @n: first decimal number
+ * @m: second decimal number
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int counter = 0;
+	unsigned long int xor_result = n ^ m;
+
+	while (xor_result > 0)
+	{
+		counter += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (counter);
+}
