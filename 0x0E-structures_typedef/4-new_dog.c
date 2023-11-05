ee4931b958edb409aac6af931ebfe68ec1928f2e
khadija shamseldin <khadija.shamseldin@gmail.com>
2023-11-04 20:09:48 -0700

    struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - created a new dog
+ * @name: char pointer
+ * @age: float value
+ * @owner: char pointer
+ * Return: a pointer to the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *husky;
+	int i, j, k;
+	
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	husky = malloc(sizeof(dog_t));
+	if (husky == NULL)
+		return (NULL);
+	for (i = 0 ; name[i] != '\0' ; i++)
+		;
+	for (j = 0 ; owner[j] != '\0' ; j++)
+		;
+	(*husky).name = malloc(sizeof(char) * i + 1);
+	if ((*husky).name == NULL)
+	{
+		free(husky);
+		return (NULL);
+	}
+	for (k = 0 ; k <= i ; k++)
+		husky->name[k] = name[k];
+	(*husky).age = age;
+	(*husky).owner = malloc(sizeof(char) * j + 1);
+	if ((*husky).owner == NULL)
+	{
+		free((*husky).name);
+		free(husky);
+		return (NULL);
+	}
+	for (k = 0 ; k <= j ; k++)
+		husky->owner[k] = owner[k];
+	return (husky);
+}
+
